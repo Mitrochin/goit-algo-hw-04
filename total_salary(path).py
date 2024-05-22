db14ab1f44151a1fa6e40e56657dbb068997400c
Mitrochin <167693989+Mitrochin@users.noreply.github.com>
2024-05-22 15:17:50 +0200

    Update total_salary(path).py

--- total_salary(path).py
+++ total_salary(path).py
@@ -1,6 +1,6 @@
 def total_salary(path):
     try:
-        with open(path, encoding='utf-8') as file:
+        with open(path, 'r', encoding='utf-8') as file:
             total_sum = 0
             num_developers = 0
             for line in file:
